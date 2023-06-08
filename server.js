a8d49706465de1901322c32ed0b4ea799f7aeac7
IvanSlivenko <ivan8822@ulr.net>
2023-06-08 22:38:30 +0300

    08-06-2023-22-38

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
-const {DB_HOST} = require("./config");
 
 
-const app = require('./app')
+
+const app = require('./app');
+
+// console.log(process.env);
+
+// const { DB_HOST } = require("./config");
+
+// ми додали ключ DB_HOST зі значенням === mongodb+srv://ivan8822:iv07092006sl@cluster0.5m4an.mongodb.net/my-movies?retryWrites=true&w=majority
+// в render environment
+const { DB_HOST } = process.env;
 
 
   // зв'язуємось з базою данних !!!
