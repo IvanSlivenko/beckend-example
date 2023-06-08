12dba56e59fb94830760b09b607b24dc8a30e7e0
IvanSlivenko <ivan8822@ulr.net>
2023-06-08 22:17:58 +0300

    08-06-2023-22-17

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+const {DB_HOST} = require("./config");
 
 
 const app = require('./app')
-// Адреса бази данних
-const DB_HOST =
-  "mongodb+srv://ivan8822:iv07092006sl@cluster0.5m4an.mongodb.net/my-movies?retryWrites=true&w=majority"
+
 
   // зв'язуємось з базою данних !!!
-mongoose.connect(DB_HOST)
-  .then(() => { 
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
     app.listen(3000);
   })
-  .catch(error => { 
+  .catch((error) => {
     console.log(error.message);
     // закриваємо всі запущені процеси
     process.exit(1);
-  })
+  });
 
 
